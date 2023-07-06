39fd510210c32525ddcc88e8ae4f1b037b7fdbf4
Kartik singh bisht <93159567+kartik01a@users.noreply.github.com>
2023-07-06 10:07:29 -0700

    v1

---
+++ project.js
@@ -0,0 +1,19 @@
+import Data from './data.js'
+
+function start(){
+    document.getElementById('mainproject').innerHTML = Data.map((data,index) => 
+        `  
+                <div key=${index} class="indi">
+                    <img class="img" src=${data.img} alt='project_img' width="400" height="240" />
+                    <span class="details">${data.name}</span>
+                    <div class="overlay">
+                        <h4>${data.desc}</h4>
+                        <a class="link" href=${data.link} target='_blank'><button class="btnn">Visit site</button></a>
+                    </div>
+                </div>
+            
+        `
+    ).join('')
+
+}
+start()
